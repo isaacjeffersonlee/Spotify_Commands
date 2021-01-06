584116212a3d439701827d678e86e21e1b778cca
isaacjeffersonlee <27ilee01@gmail.com>
2021-01-06 17:26:52 +0000

    Added config file

---
+++ empty_spcom_config.py
@@ -0,0 +1,14 @@
+# Spcom Config File
+# To get details go to:
+#https://developer.spotify.com/dashboard/login
+# create a new app and edit settings, add a redirect uri which
+# can be any website, then make note of client secret and client id
+# and add them below.
+# username can be gotten from the spotify app itself in settings.
+# e.g username = 'username?si=AksdksjalkdAKSkjs'
+username = 
+client_secret = 
+client_id = 
+# The url you are sent to to verify, must match spotify
+# app in developers dashboard redirect uri
+redirect_uri = 'https://www.google.com/'
